cf307820743ffd74780ec267ae8e1e8b88abfda2
Anders <anders.erik.lie@gmail.com>
2021-04-13 20:15:34 -0500

    cute plotting

--- test_example.py
+++ test_example.py
@@ -4,14 +4,22 @@ import datetime as dt
 import pandas_datareader as pdr
 
 fig = plt.figure()
+ax = fig.add_subplot()
+
+MAX = 20
 
 spy = pdr.DataReader('SPY', 'yahoo', dt.datetime(2020,1,1), dt.datetime(2021,1,1))
 print(spy.head())
 
 def animate(i):
-    plt.plot(i, spy['Close'][i],'ro')
+    x = spy.index[max(0, i-MAX):i]
+    y = spy['Close'][max(0, i-MAX):i]
+    ax.clear()
+    ax.plot(x, y)
+
+    #plt.plot(spy.index[i], spy['Close'][i], 'ro')
     print(i)
 
-anim = FuncAnimation(fig, animate, interval=1000)
+anim = FuncAnimation(fig, animate, interval=100)
 
 plt.show()

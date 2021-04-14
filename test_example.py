af2c163856b4a9dee9e584aa3d0da78566e9dd75
Anders <anders.erik.lie@gmail.com>
2021-04-13 20:01:55 -0500

    spy example

--- test_example.py
+++ test_example.py
@@ -1,10 +1,15 @@
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
+import datetime as dt
+import pandas_datareader as pdr
 
 fig = plt.figure()
 
+spy = pdr.DataReader('SPY', 'yahoo', dt.datetime(2020,1,1), dt.datetime(2021,1,1))
+print(spy.head())
+
 def animate(i):
-    plt.plot(0,i,'ro')
+    plt.plot(i, spy['Close'][i],'ro')
     print(i)
 
 anim = FuncAnimation(fig, animate, interval=1000)
